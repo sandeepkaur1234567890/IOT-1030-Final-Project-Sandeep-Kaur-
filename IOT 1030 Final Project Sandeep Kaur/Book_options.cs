930cff5e151afa1bbf998cc977aa7944bd5e077c
sandeepkaur1234567890 <78116974+sandeepkaur1234567890@users.noreply.github.com>
2021-12-15 20:57:52 -0500

    first commit

---
+++ IOT 1030 Final Project Sandeep Kaur/Book_options.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOT_1030_Final_Project_Sandeep_Kaur
+{
+    public class Book_options
+    {
+        public int ID { get; set; }
+        public string Book_name { get; set; }
+        public string Author_name { get; set; }
+        public string Book_Category { get; set; }
+        public double Book_price { get; set; }
+        public double ISBN_number { get; set; }
+        public DateTime Year_of_Lunched { get; set; }
+
+        public Book_options(int id, string book_name, string author_name, string book_category, double price, double isbn, DateTime year_of_lunched)
+        {
+            ID = id;
+            Book_name = book_name;
+            Author_name = author_name;
+            Book_Category = book_category;
+            Book_price = price;
+            ISBN_number = isbn;
+            Year_of_Lunched = year_of_lunched;
+        }
+
+        public Book_options()
+        { }
+    }
+}
---
+++ IOT 1030 Final Project Sandeep Kaur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOT_1030_Final_Project_Sandeep_Kaur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
